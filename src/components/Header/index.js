01c5f3d29c8acb73600ec3161593d88ade254abc
Resi Respati <resir014@gmail.com>
2018-01-18 19:34:56 +0700

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = () => (
+  <div>
+    <div>
+      <h1>
+        <Link to="/">Gatsby</Link>
+      </h1>
+    </div>
+  </div>
+)
+
+export default Header
