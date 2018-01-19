a3f0896491eeb734acc90cab57e7c60c130843a5
Resi Respati <resir014@gmail.com>
2018-01-19 07:40:12 +0700

    include normalize

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import 'normalize.css'
+
 import Header from '../components/Header'
 
 const TemplateWrapper = ({ children }) => (
